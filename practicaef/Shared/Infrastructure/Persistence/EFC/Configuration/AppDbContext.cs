91632ac1061c6d3b77b4af708eed3a141cd03e27
Al3xiel <gabriel280104casas@gmail.com>
2024-11-28 15:41:37 -0500

    feat(db):  added database construction.

--- practicaef/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ practicaef/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,6 +1,8 @@
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
 using practicaef.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using practicaef.sms.Domain.Model.Aggregates;
+using practicaef.wms.Domain.Model.Aggregates;
 
 namespace practicaef.Shared.Infrastructure.Persistence.EFC.Configuration;
 
@@ -16,7 +18,21 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         base.OnModelCreating(builder);
         
+        builder.Entity<OrderItem>().HasKey(o => o.Id);
+        builder.Entity<OrderItem>().Property(o => o.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<OrderItem>().Property(o => o.EpicorSku).IsRequired();
+        builder.Entity<OrderItem>().Property(o => o.Status).IsRequired();
+        builder.Entity<OrderItem>().Property(o => o.RequestedQuantity).IsRequired();
+        builder.Entity<OrderItem>().Property(o => o.OrderedAt).IsRequired();
         
+        builder.Entity<InventoryItem>().HasKey(i => i.Id);
+        builder.Entity<InventoryItem>().Property(i => i.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<InventoryItem>().Property(i => i.EpicorSku).IsRequired();
+        builder.Entity<InventoryItem>().Property(i => i.Status).IsRequired();
+        builder.Entity<InventoryItem>().Property(i => i.MinimumQuantity).IsRequired();
+        builder.Entity<InventoryItem>().Property(i => i.AvailableQuantity).IsRequired();
+        builder.Entity<InventoryItem>().Property(i => i.ReservedQuantity).IsRequired();
+        builder.Entity<InventoryItem>().Property(i => i.PendingSupplyQuantity).IsRequired();
         
         builder.UseSnakeCaseNamingConvention();
     }
