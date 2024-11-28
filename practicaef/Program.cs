f2a047711a28ce4797facc62bfc4c33021d5b9ea
Al3xiel <gabriel280104casas@gmail.com>
2024-11-28 12:44:17 -0500

    chore: added program.cs configuration.

--- practicaef/Program.cs
+++ practicaef/Program.cs
@@ -1,13 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using practicaef.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using practicaef.Shared.Infrastructure.Persistence.EFC.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (connectionString is null)
+    throw new Exception("Connection string is null.");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableDetailedErrors()
+            .EnableSensitiveDataLogging();
+    else if (builder.Environment.IsProduction())
+        options.UseMySQL(connectionString);
+});
+
+// OpenAPI/Swagger Configuration
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+//Dependency Injection Configuration
+//TODO:ADD DEPENDENCY INJECTION CONFIGURATION
 
 var app = builder.Build();
+
+// Verify Database Objects are Created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
